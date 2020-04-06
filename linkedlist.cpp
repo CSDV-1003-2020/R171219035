3db0740057d1c5288e9066c6d2e9d0da5fa2db82
SnigdhaMakanday <59870580+SnigdhaMakanday@users.noreply.github.com>
2020-04-07 07:09:55 +1300

    adding a conditional statement and added a push function to the code

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -37,4 +37,9 @@ void insertAfter(Node* prev_node, int new_data)
 /* Given a reference (pointer to pointer) 
 to the head of a list and an int, inserts 
 a new node on the front of the list. */
-void push(Node** head_ref, int new_data)
+void push(Node** head_ref, int new_data)/*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)  
+    {  
+        cout<<"the given previous node cannot be NULL";  
+        return;  
+    }  
