7645bbd3ed0f50cac5aa252be7617d263018965b
ShlokMohanty <59838234+ShlokMohanty@users.noreply.github.com>
2020-04-06 23:24:06 +0530

    data and next pointer
    added the data or the information  and the reference pointer to the next node in the created node

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -6,3 +6,13 @@ class Node
     int data;  
     Node *next;  
 };  
+void push(Node** head_ref, int new_data)  
+{  
+    Node* new_node = new Node();  
+ 
+    new_node->data = new_data;  
+  
+    new_node->next = (*head_ref);  
+ 
+    (*head_ref) = new_node;  
+}  
