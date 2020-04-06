efbeb0a86f6a3cbe37df028e0a9541f5b9f3f4a2
SnigdhaMakanday <59870580+SnigdhaMakanday@users.noreply.github.com>
2020-04-07 07:03:40 +1300

    node insertion function

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -16,3 +16,24 @@ void push(Node** head_ref, int new_data)
  
     (*head_ref) = new_node;  
 }  
+void insertAfter(Node* prev_node, int new_data)  
+{  
+    /*1. check if the given prev_node is NULL */
+    if (prev_node == NULL)  
+    {  
+        cout<<"the given previous node cannot be NULL";  
+        return;  
+    }  
+  
+    /* 2. allocate new node */
+    Node* new_node = new Node(); 
+  
+    /* 3. put in the data */
+    new_node->data = new_data;  
+  
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;  
+  
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;  
+}  
