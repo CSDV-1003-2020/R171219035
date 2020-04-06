4b8422292a3972b9ff55d3242c76c228ac718470
SnigdhaMakanday <59870580+SnigdhaMakanday@users.noreply.github.com>
2020-04-07 07:19:48 +1300

    completed the insert After function .

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -50,4 +50,12 @@ void insertAfter(Node* prev_node, int new_data)
   
     /* 3. put in the data */
     new_node->data = new_data;  
-     
+     /* 3. put in the data */
+    new_node->data = new_data;  
+  
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;  
+  
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;  
+}  
