d533a039e726300e700e99288a6a1ba34956ac9d
SnigdhaMakanday <59870580+SnigdhaMakanday@users.noreply.github.com>
2020-04-07 17:04:48 +1300

    added a push function

--- delll.cpp
+++ delll.cpp
@@ -9,3 +9,13 @@ struct Node
     int data; 
     struct Node *next; 
 }; 
+/* Given a reference (pointer to pointer) to the head of a list 
+   and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data) 
+{ 
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+    new_node->data  = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref)    = new_node; 
+} 
+  
