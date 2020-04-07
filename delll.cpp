2ca8bfb5f5b08b305c1c2fd21fb9e3b728888378
Shlok <shlokmohanty2000@gmail.com>
2020-04-07 09:31:16 +0530

    constructed a node

---
+++ delll.cpp
@@ -0,0 +1,11 @@
+// A complete working C program to demonstrate deletion in singly 
+// linked list 
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+// A linked list node 
+struct Node 
+{ 
+    int data; 
+    struct Node *next; 
+}; 
